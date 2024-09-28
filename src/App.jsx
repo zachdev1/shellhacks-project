f693741f33b6a3cbe8ffeea982571b78d85ac826
zach <zmsoftdev@gmail.com>
2024-09-28 00:33:46 -0400

    typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.css";
+import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./components/Dashboard";
