354d0ba74bb09761011a159bd9b7934619018d65
zach <zmsoftdev@gmail.com>
2024-09-29 10:42:47 -0400

    toggle sidebar and styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/sidebar.css";
 
 const Sidebar = () => {
-  return (
-    <nav className="sidebar">
-      <ul>
-        <li>
-          <Link to="/">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
 
-        <li>
-          <Link to="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link to="/transactions">Transactions</Link>
-        </li>
-        <li>
-          <Link to="/budget-planner">Budget Planner</Link>
-        </li>
-        <li>
-          <Link to="/assets">Assets</Link>
-        </li> 
-        <li>
-          <Link to="/reports">*Reports</Link>
-        </li>
-      </ul>
-    </nav>
+  const [sidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!sidebarOpen);
+  };
+
+
+  return (
+    <div>
+    <button className="sidebarToggle" onClick={toggleSidebar}>Toggle</button>
+    {sidebarOpen && (
+      <nav className="sidebar">
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/transactions">Transactions</Link>
+          </li>
+          <li>
+            <Link to="/budget-planner">Budget Planner</Link>
+          </li>
+          <li>
+            <Link to="/assets">Assets</Link>
+          </li> 
+          <li>
+            <Link to="/reports">*Reports</Link>
+          </li>
+        </ul>
+      </nav>
+    )}
+    </div>
+    
   );
 };
 
